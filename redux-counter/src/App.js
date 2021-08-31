057ed6337d1f2e2ac9474e3c8e7b89f873611a7a
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-08-31 08:45:02 +0300

    updated app structure

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -1,10 +1,18 @@
+import { Fragment } from 'react';
 import Counter from './components/Counter';
-
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <Counter />
-  );
+	return (
+		<Fragment>
+			<Header />
+			<Auth />
+			{/* <UserProfile /> */}
+			<Counter />
+		</Fragment>
+	);
 }
 
 export default App;
